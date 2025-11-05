df9f1f4c9b270f58cca9097013b21343bbec96f8
Yomioh10 <contact.teckma@gmail.com>
2025-11-05 03:06:53 +0100

    Frontend sample list product

---
+++ frontend/src/data.ts
@@ -0,0 +1,39 @@
+import type { Product } from "./types/Product";
+export const sampleProducts: Product[] = [
+   {
+      name: "BELLA AURORA ecran spf50 peau mixte à grasse",
+      slug: "bella-aurora-ecran-spf50-peau-mixte-a-grasse",
+      image: "/images/bella-aurora-ecran-spf50-peau-mixte-a-grasse.jpg",
+      category: "Ecrans solaires",
+      brand: "BELLA AURORA",
+      price: 188.00,
+      countInStock: 10,
+      description: "L'écran solaire SPF50 pour les peaux mixtes et grasses offre une protection maximale. Sa formule améliorée avec des filtres UVA, UVB et IR à large spectre, microencapsulés dans un système à double sphère, vous garantit une protection maximale contre l'exposition au soleil. Réduction des taches cutanées.Sa formulation comprend de puissants agents dépigmentants qui traitent les imperfections existantes, les réduisent visiblement et empêchent l'apparition de nouvelles. Apaise la peau pendant l'exposition au soleil et réduit l'inflammation. Appliquer toutes les deux heures.",
+      conseils: "Appliquer généreusement sur le visage avant l'exposition au soleil. Renouveler l'application toutes les deux heures et après chaque baignade.",
+      numReviews: 12
+   },
+   {
+      name: "DCP TRIO ACNE gel nettoyant moussant 200 ml",
+      slug: "dcp-trio-acne-gel-nettoyant-moussant-200-ml",
+      image: "/images/dcp-trio-acne-gel-nettoyant-moussant-200-ml.jpg",
+      category: "Démaquillants & nettoyants",
+      brand: "DCP",
+      price: 166.32,
+      countInStock: 20,
+      description: "Spécialement conçu pour nettoyer en douceur les peaux mixtes et grasses à imperfections, dcp TRIO ACNE Gel prévient les éruptions cutanées, favorise l'élimination des impuretés, purifie et assainit l'épiderme tout en nettoyant en douceur la peau",
+      conseils: "Appliquer matin et soir, DCP TRIOACNE GEL sur peau humide faire mousser avec un peu d'eau rincer abondamment à l'eau puis sécher sans frotter Idéal pour toutes les zones touchées par l'acné y compris le visage le dos et poitrine",
+      numReviews: 6
+   },
+   {
+      name: "SANTAROME BIO VENTRE PLAT double action ciblée 30 gélules",
+      slug: "santarome-bio-ventre-plat-double-action-ciblee-30-gelules",
+      image: "/images/santarome-bio-ventre-plat-double-action-ciblee-30-gelules.jpg",
+      category: "Compléments alimentaires",
+      brand: "SANTAROME",
+      price: 175.00,
+      countInStock: 20,
+      description: "Découvrez Santarome Bio Ventre Plat Double Action Ciblée 30 Gélules, votre allié pour un ventre plat et une silhouette affinée. Ce complément alimentaire innovant se distingue par sa formule double action, élaborée à partir d’ingrédients bio de haute qualité pour vous aider à atteindre vos objectifs de bien-être.",
+      conseils: "Pour des résultats optimaux, prenez 2 gélules par jour avec un repas et un grand verre d’eau (20 cl). Il est déconseillé de prendre les gélules en dehors des repas en raison de la présence de thé vert. Ne dépassez pas la dose quotidienne recommandée. Contient de la caféine (6 mg/2 gélules) : déconseillé aux enfants, adolescents, femmes enceintes ou allaitantes. Déconseillé aux personnes sous traitement anticoagulant, souffrant d'allergie aux dérivés salicylés ou de troubles thyroïdiens en raison de l’iode.",
+      numReviews: 8
+   }
+]
