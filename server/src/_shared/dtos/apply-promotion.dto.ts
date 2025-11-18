9468c5b45aefc5c1d6ab61d6b3db139aee1c400f
Yomioh10 <contact.teckma@gmail.com>
2025-11-18 11:21:12 +0100

    commit changes

--- server/src/_shared/dtos/apply-promotion.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsString, IsNotEmpty } from 'class-validator';
-
-export class ApplyPromotionDto {
-  @IsString()
-  @IsNotEmpty({ message: 'The couponCode field is required.' })
-  // The promotion code submitted by the user (e.g., "GET25OFF")
-  couponCode: string; 
-}
